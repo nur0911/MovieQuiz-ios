dbdf301c809020f56e2d10d159e253c139ca4a7f
IITU <nur0911@mail.ru>
2024-04-25 18:12:01 +0500

    изменен

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,7 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool 
    {
-       print("Hello world")
+       print("Hello world for branch to merge")
         return true
     }
 
